d057528c61993d15b2569478356f4f3907274032
alexisdlr <alexisdlr3009@hotmail.com>
2024-01-19 16:33:34 -0600

    header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,8 +2,8 @@ import SwitcherTheme from "./SwitcherTheme";
 
 const Header = () => {
   return (
-    <div className="w-full h-[130px] lg:h-[160px] relative mx-auto my-0">
-      <div className="w-full h-full lg:h-[160px] lg:rounded-bl-full bg-violet" >
+    <div className="w-full h-[130px] lg:h-[160px] relative bg-[url('/assets/header.png')] mx-auto my-0">
+      <div className="w-full h-full lg:h-[160px] lg:rounded-bl-full" >
         <div className="w-[80%] md:w-full md:max-w-3xl lg:max-w-5xl lg:px-0 max-w-lg h-2/3 mx-auto flex items-center justify-between">
           <h1 className="text-white text-xl md:text-2xl lg:text-3xl font-bold">devjobs</h1>
           <span><SwitcherTheme /></span>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,22 +1,14 @@
 import NavbarMobile from "./NavbarMobile";
 import { FaSearch } from "react-icons/fa";
 import { HiMiniMapPin } from "react-icons/hi2";
+import SearchInput from "./SearchInput";
 
 export default function Navbar() {
   return (
     <div className="w-[80%] md:w-full md:max-w-3xl lg:max-w-5xl h-[80px] absolute inset-x-0 bg-white dark:bg-verydarkblue bottom-[-30px] rounded-[6px] mx-auto my-0 flex shadow-lg">
       <NavbarMobile />
       <div className=" w-1/3 h-full hidden md:flex items-center justify-start border-r p-6 border-gray">
-        <div className="w-full flex items-center text-sm 2xl:text-base">
-          <span>
-            <FaSearch size={24} className="text-violet" />
-          </span>
-          <input
-            type="text"
-            className="w-full text-verydarkblue bg-transparent outline-none border-none pl-2 text-xs lg:text-sm"
-            placeholder="Filter by name, companie, expertise..."
-          />
-        </div>
+        <SearchInput />
       </div>
       <div className=" w-1/3 h-full hidden md:flex items-center justify-start border-r p-6 border-gray">
         <div className="w-full flex items-center text-sm 2xl:text-base">
